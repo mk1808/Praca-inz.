85519fac683bcf179158bd4735836e203d4dcaac
mk1808 <monikor97@gmail.com>
2019-06-10 21:36:39 +0200

    #10 testowanie galerii

--- View/inz-app/src/app/test/test.component.ts
+++ View/inz-app/src/app/test/test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -9,7 +9,57 @@ export class TestComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
+  ngOnInit(): void {
+
+      this.galleryOptions = [
+          {
+              width: '600px',
+              height: '400px',
+              thumbnailsColumns: 4,
+              imageAnimation: NgxGalleryAnimation.Slide
+          },
+          // max-width 800
+          {
+              breakpoint: 800,
+              width: '100%',
+              height: '600px',
+              imagePercent: 80,
+              thumbnailsPercent: 20,
+              thumbnailsMargin: 20,
+              thumbnailMargin: 20
+          },
+          // max-width 400
+          {
+              breakpoint: 400,
+              preview: false
+          }
+      ];
+
+      this.galleryImages = [
+          {
+              small: 'https://picsum.photos/50',
+              medium: 'https://picsum.photos/100/200',
+              big: 'https://picsum.photos/200/300'
+          },
+          {
+              small: 'https://picsum.photos/50',
+              medium: 'https://picsum.photos/100/200',
+              big: 'https://picsum.photos/200/300'
+          },
+          {
+              small: 'https://picsum.photos/50',
+              medium: 'https://picsum.photos/100/200',
+              big: 'https://picsum.photos/200/300'
+          },
+          {
+            small: 'https://picsum.photos/50',
+            medium: 'https://picsum.photos/100/200',
+            big: 'https://picsum.photos/200/300'
+        } 
+
+      ];
+  }
 }
