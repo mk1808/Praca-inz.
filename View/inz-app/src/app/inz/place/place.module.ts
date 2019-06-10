88c2154b6be06926a98f1c495970afc9a4033182
mk1808 <monikor97@gmail.com>
2019-06-11 01:14:08 +0200

    #10 dodanie galerii, tytułu, części informacji

--- View/inz-app/src/app/inz/place/place.module.ts
+++ View/inz-app/src/app/inz/place/place.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaceComponent } from './place/place.component';
 import { PlaceRoutingModule } from './place-routing.module';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 @NgModule({
   declarations: [PlaceComponent],
   imports: [
     CommonModule,
-    PlaceRoutingModule
+    PlaceRoutingModule,
+    NgxGalleryModule
+    
   ]
 })
 export class PlaceModule { }
--- View/inz-app/src/app/inz/place/place/place.component.ts
+++ View/inz-app/src/app/inz/place/place/place.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-place',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  ngOnInit(): void {
+
+      this.galleryOptions = [
+          {
+              width: '600px',
+              height: '400px',
+              thumbnailsColumns: 4,
+              imageAnimation: NgxGalleryAnimation.Slide
+          },
+          // max-width 800
+          {
+              breakpoint: 800,
+              width: '100%',
+              height: '600px',
+              imagePercent: 80,
+              thumbnailsPercent: 20,
+              thumbnailsMargin: 20,
+              thumbnailMargin: 20
+          },
+          // max-width 400
+          {
+              breakpoint: 400,
+              preview: false
+          }
+      ];
+
+      this.galleryImages = [
+          {
+              small: 'https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+              medium: 'https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+              big: 'https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500'
+          },
+          {
+              small: 'https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+              medium: 'https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+              big: 'https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500'
+          },
+          {
+              small: 'https://images.pexels.com/photos/53350/seoul-skyscraper-building-architecture-53350.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+              medium: 'https://images.pexels.com/photos/53350/seoul-skyscraper-building-architecture-53350.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+              big: 'https://images.pexels.com/photos/53350/seoul-skyscraper-building-architecture-53350.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+          },
+          {
+            small: 'https://images.pexels.com/photos/417273/pexels-photo-417273.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+            medium: 'https://images.pexels.com/photos/417273/pexels-photo-417273.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+            big: 'https://images.pexels.com/photos/417273/pexels-photo-417273.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500'
+        } 
+
+      ];
   }
-
 }
