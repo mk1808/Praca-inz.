4e32b48609e9f90d10d70f66e351934c47dfdacc
mk1808 <monikor97@gmail.com>
2019-06-14 15:06:56 +0200

    #11 tabela - początek

--- View/inz-app/src/app/inz/trip/trip.module.ts
+++ View/inz-app/src/app/inz/trip/trip.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripComponent } from './trip/trip.component';
 import { TripRoutingModule } from './trip-routing.module';
+import { PhotosOnHoverComponent } from './trip/photos-on-hover/photos-on-hover.component';
 
 @NgModule({
-  declarations: [TripComponent],
+  declarations: [TripComponent, PhotosOnHoverComponent],
   imports: [
     CommonModule,
     TripRoutingModule
--- View/inz-app/src/app/inz/trip/trip/trip.component.ts
+++ View/inz-app/src/app/inz/trip/trip/trip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ComponentsService } from 'src/app/shared/services/components.service';
 
 @Component({
   selector: 'app-trip',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip.component.scss']
 })
 export class TripComponent implements OnInit {
-
-  constructor() { }
+hover=false;
+tableContent:any[] = [
+  {id: 1, name: "miejsce001", location:"Warszawa"},
+  {id: 2, name: "miejsce002", location:"Warszawa"},
+  {id: 3, name: "miejsce003", location:"Warszawa"},
+  {id: 4, name: "miejsce004", location:"Warszawa"},
+  {id: 5, name: "miejsce005", location:"Warszawa"}, 
+];
+  constructor(private componentsService:ComponentsService) { }
 
   ngOnInit() {
+    this.hover=false;
   }
 
+onHover(i){
+  console.log(i);
+  console.log(this.tableContent[i]);
+  this.hover=true;
+  let item=this.tableContent[i];
+  this.componentsService.setTableItem(item);
+}
 }
