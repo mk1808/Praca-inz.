8ca8435875d860030daa3a9ae8dfa50b7b0b20c2
mk1808 <monikor97@gmail.com>
2019-07-01 12:20:55 +0200

    #11 dodanie zdjec i poprawkii

--- View/inz-app/src/app/inz/trip/trip/trip.component.ts
+++ View/inz-app/src/app/inz/trip/trip/trip.component.ts
@@ -11,10 +11,10 @@ hover=false;
 
 tableContent:any[] = [
   {id: 1, name: "Muzeum Narodowe", location:"Warszawa", photo:"https://rosenthalblog.files.wordpress.com/2019/02/muzeum-narodowe.jpg"},
-  {id: 2, name: "miejsce002", location:"Warszawa", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Warszawa-Zamek_Kr%C3%B3lewski.jpg/1024px-Warszawa-Zamek_Kr%C3%B3lewski.jpg"},
-  {id: 3, name: "miejsce003", location:"Warszawa", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Palac_Lazienki%2CWarszawa%2CPolska%2CUE._-_panoramio_%2841%29.jpg/800px-Palac_Lazienki%2CWarszawa%2CPolska%2CUE._-_panoramio_%2841%29.jpg"},
-  {id: 4, name: "miejsce004", location:"Warszawa", photo:"https://www.defor.eu/wp-content/uploads/2018/04/1cnk5.jpg"},
-  {id: 5, name: "miejsce005", location:"Warszawa", photo:"https://images.pexels.com/photos/77382/palac-kultury-palace-culture-pkin-kinoteka-77382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}, 
+  {id: 2, name: "Zamek Królewski", location:"Warszawa", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Warszawa-Zamek_Kr%C3%B3lewski.jpg/1024px-Warszawa-Zamek_Kr%C3%B3lewski.jpg"},
+  {id: 3, name: "Łazienki", location:"Warszawa", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Palac_Lazienki%2CWarszawa%2CPolska%2CUE._-_panoramio_%2841%29.jpg/800px-Palac_Lazienki%2CWarszawa%2CPolska%2CUE._-_panoramio_%2841%29.jpg"},
+  {id: 4, name: "Muzeum Fryderyka Chopina", location:"Warszawa", photo:"https://ocs-pl.oktawave.com/v1/AUTH_f9dd5582-c0b6-4b27-a573-ecf06cf3ef09/tropter-www/uploads/images/72/20/c3d07d4d4b89d695216730eb8d6fadcc4deb/muzeum_fryderykachopina_000_medium.jpeg?t=20181211080851"},
+  {id: 5, name: "Pałac Kultury i Nauki", location:"Warszawa", photo:"https://images.pexels.com/photos/77382/palac-kultury-palace-culture-pkin-kinoteka-77382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}, 
 ];
   constructor(private componentsService:ComponentsService) { }
   latitude = 50.026783;
