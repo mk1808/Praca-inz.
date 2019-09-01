5dfae952cb3fd9d7b6780aeada0c94264eccedd8
Monika Kordoń <monikor97@gmail.com>
2019-09-01 22:02:22 +0200

    testy

--- View/inz-app/src/app/test/test.component.ts
+++ View/inz-app/src/app/test/test.component.ts
@@ -1,65 +1,137 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+    selector: 'app-test',
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+    todo = [
+        'Get to work',
+        'Pick up groceries',
+        'Go home',
+        'Fall asleep'
+    ];
 
-  constructor() { }
+    done = [
+        'Get up',
+        'Brush teeth',
+        'Take a shower',
+        'Check e-mail',
+        'Walk dog'
+    ];
 
-  galleryOptions: NgxGalleryOptions[];
-  galleryImages: NgxGalleryImage[];
+    next = [
+        'Get up2',
+        'Brush teeth2',
+        'Take a shower2',
+        'Check e-mail2',
+        'Walk dog2'
+    ];
 
-  ngOnInit(): void {
+    drop(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
 
-      this.galleryOptions = [
-          {
-              width: '600px',
-              height: '400px',
-              thumbnailsColumns: 4,
-              imageAnimation: NgxGalleryAnimation.Slide
-          },
-          // max-width 800
-          {
-              breakpoint: 800,
-              width: '100%',
-              height: '600px',
-              imagePercent: 80,
-              thumbnailsPercent: 20,
-              thumbnailsMargin: 20,
-              thumbnailMargin: 20
-          },
-          // max-width 400
-          {
-              breakpoint: 400,
-              preview: false
-          }
-      ];
+    dropn(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
 
-      this.galleryImages = [
-          {
-              small: 'https://picsum.photos/50',
-              medium: 'https://picsum.photos/100/200',
-              big: 'https://picsum.photos/200/300'
-          },
-          {
-              small: 'https://picsum.photos/50',
-              medium: 'https://picsum.photos/100/200',
-              big: 'https://picsum.photos/200/300'
-          },
-          {
-              small: 'https://picsum.photos/50',
-              medium: 'https://picsum.photos/100/200',
-              big: 'https://picsum.photos/200/300'
-          },
-          {
-            small: 'https://picsum.photos/50',
-            medium: 'https://picsum.photos/100/200',
-            big: 'https://picsum.photos/200/300'
-        } 
 
-      ];
-  }
+
+    timePeriods = [
+        'Bronze age',
+        'Iron age',
+        'Middle ages',
+        'Early modern period',
+        'Long nineteenth century'
+    ];
+
+    drop2(event: CdkDragDrop<string[]>) {
+        moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+
+
+        {
+            if (event.previousContainer === event.container) {
+                moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+            } else {
+                transferArrayItem(event.previousContainer.data,
+                    event.container.data,
+                    event.previousIndex,
+                    event.currentIndex);
+            }
+        }
+    }
+    constructor() { }
+
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
+
+    ngOnInit(): void {
+
+        this.galleryOptions = [
+            {
+                width: '600px',
+                height: '400px',
+                thumbnailsColumns: 4,
+                imageAnimation: NgxGalleryAnimation.Slide
+            },
+            // max-width 800
+            {
+                breakpoint: 800,
+                width: '100%',
+                height: '600px',
+                imagePercent: 80,
+                thumbnailsPercent: 20,
+                thumbnailsMargin: 20,
+                thumbnailMargin: 20
+            },
+            // max-width 400
+            {
+                breakpoint: 400,
+                preview: false
+            }
+        ];
+
+        this.galleryImages = [
+            {
+                small: 'https://picsum.photos/50',
+                medium: 'https://picsum.photos/100/200',
+                big: 'https://picsum.photos/200/300'
+            },
+            {
+                small: 'https://picsum.photos/50',
+                medium: 'https://picsum.photos/100/200',
+                big: 'https://picsum.photos/200/300'
+            },
+            {
+                small: 'https://picsum.photos/50',
+                medium: 'https://picsum.photos/100/200',
+                big: 'https://picsum.photos/200/300'
+            },
+            {
+                small: 'https://picsum.photos/50',
+                medium: 'https://picsum.photos/100/200',
+                big: 'https://picsum.photos/200/300'
+            }
+
+        ];
+    }
+
+   
 }
