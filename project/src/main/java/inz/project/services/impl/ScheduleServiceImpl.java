df48b4d7d7a0f56e32163d6825550cb4c94e6cde
Monika Kordoń <monikor97@gmail.com>
2019-09-02 00:07:11 +0200

    #23 endpointy - create, update- cd

--- project/src/main/java/inz/project/services/impl/ScheduleServiceImpl.java
+++ project/src/main/java/inz/project/services/impl/ScheduleServiceImpl.java
@@ -3,6 +3,8 @@ package inz.project.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import inz.project.models.Schedule;
+import inz.project.models.User;
 import inz.project.repositories.ScheduleRepository;
 import inz.project.services.ScheduleService;
 
@@ -10,4 +12,16 @@ import inz.project.services.ScheduleService;
 public class ScheduleServiceImpl implements ScheduleService{
 
 	@Autowired ScheduleRepository scheduleRepository;
+	
+	@Override
+	public Schedule createSchedule(Schedule schedule) {
+		return this.scheduleRepository.save(schedule);
+	}
+	
+	@Override
+	public Schedule updateSchedule(Long id, Schedule schedule) {
+		schedule.setId(id);
+		Schedule updated = this.scheduleRepository.save(schedule);
+		return updated;
+	}
 }
