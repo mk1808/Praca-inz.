b9e3a48634ec0d01556746c909e9f4b554890878
Monika Kordoń <monikor97@gmail.com>
2019-09-06 21:43:47 +0200

    #13 dodanie formularza

--- View/inz-app/src/app/inz/main/all-places/all-places.component.ts
+++ View/inz-app/src/app/inz/main/all-places/all-places.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Place } from 'src/app/shared/models/classes';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PlaceService } from 'src/app/shared/services/place.service';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -19,10 +19,18 @@ export class AllPlacesComponent implements OnInit {
   stateCtrl = new FormControl();
   filteredStates: Observable<any[]>;
   initialized:boolean = false;
-  constructor(private router: Router, private route: ActivatedRoute, private placeService: PlaceService) {
+  form:FormGroup;
 
+  constructor(private router: Router, private route: ActivatedRoute, private placeService: PlaceService,
+    private fb: FormBuilder) {
 
-    this.stateCtrl.valueChanges.subscribe(y=>{
+      this.form = this.fb.group({
+        name:[null],
+        category:[null],
+        region:[null]});  
+         this.initialized=true;
+
+    this.form.controls.name.valueChanges.subscribe(y=>{
       console.log(y);
       this.placeService.getPlacesFiltered(y).subscribe(x=>{
         this.filteredPlaces = x;
@@ -45,10 +53,11 @@ export class AllPlacesComponent implements OnInit {
 
   ngOnInit() {
 
+
     this.placeService.getPlaces().subscribe(x => {
       this.places = x;
       console.log(this.places);
-    this.initialized=true;
+
     })
   
 
