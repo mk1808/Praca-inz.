bd38a20012f7632ca8703e4b3d284f87e95041e1
Monika Kordoń <monikor97@gmail.com>
2019-09-28 23:00:14 +0200

    #24 dodanie tabeli

--- View/inz-app/src/app/inz/place/add-place/add-place.component.ts
+++ View/inz-app/src/app/inz/place/add-place/add-place.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { TripService } from 'src/app/shared/services/trip.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User, Trip } from 'src/app/shared/models/classes';
 
 @Component({
   selector: 'app-add-place',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-place.component.scss']
 })
 export class AddPlaceComponent implements OnInit {
-
-  constructor() { }
+user:User=new User;
+myTrips:Trip[]=[];
+  constructor(private router: Router, private route: ActivatedRoute, private cookie: CookieService,
+    private tripService: TripService) { }
 
   ngOnInit() {
+
+    if (this.cookie.get('user') == "") {
+      this.router.navigate(['/']);
+
+    } else {
+      this.user = JSON.parse(this.cookie.get('user'));
+      this.tripService.getTripsByUser(this.user.id).subscribe(x => {
+        this.myTrips = x;
+        console.log(this.myTrips);
+      })
+    }
   }
+
 
 }
