5b1963ea4603f675057084f9209b7c61ddcd2283
Monika Kordoń <monikor97@gmail.com>
2019-10-06 22:34:43 +0200

    #26 podpiecie metod do serwisu schedule

--- View/inz-app/src/app/shared/services/schedule.service.ts
+++ View/inz-app/src/app/shared/services/schedule.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { RestService } from './rest.service';
-import { Schedule } from '../models/classes';
+import { Schedule, PositionInSchedule } from '../models/classes';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -16,5 +16,14 @@ export class ScheduleService {
   public updateSchedule(schedule:Schedule): Observable<any> { 
   
     return this.rest.put<any>('/api/schedule/'+schedule.id,schedule );
+  }
+
+  public updatePositionInSchedule(position:PositionInSchedule): Observable<any> { 
+  
+    return this.rest.put<any>('/api/positionInSchedule', position );
+  }
+
+  public getPositionInSchedule(placeId:number, tripId:number){
+    return this.rest.get<any>('/api/positionInSchedule/place/'+placeId+"/trip/"+tripId)
   }
 }
