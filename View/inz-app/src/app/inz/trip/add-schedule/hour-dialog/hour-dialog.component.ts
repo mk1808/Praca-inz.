1d3cbcf23f3326dae7d5bb763fe71eb32eb11736
Monika Kordoń <monikor97@gmail.com>
2019-10-06 17:53:27 +0200

    #26 dodanie dialogu z wyborem godziny

---
+++ View/inz-app/src/app/inz/trip/add-schedule/hour-dialog/hour-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-hour-dialog',
+  templateUrl: './hour-dialog.component.html',
+  styleUrls: ['./hour-dialog.component.scss']
+})
+export class HourDialogComponent implements OnInit {
+  form: FormGroup;
+  constructor(   private formBuilder: FormBuilder,
+    private dialogRef: MatDialogRef<HourDialogComponent>) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      filename:''
+    })
+  }
+
+}
