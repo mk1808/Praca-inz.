c0e915e4ba75b3e5b2f95acd31dfe30cbe55f218
Monika Kordoń <monikor97@gmail.com>
2019-10-06 19:23:48 +0200

    #26 dodawanie pozycji w grafiku - backend - controller

--- project/src/main/java/inz/project/controllers/PositionInScheduleController.java
+++ project/src/main/java/inz/project/controllers/PositionInScheduleController.java
@@ -1,8 +1,12 @@
 package inz.project.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import inz.project.models.PositionInSchedule;
 import inz.project.services.PositionInScheduleService;
 import inz.project.services.PositionInTripService;
 import inz.project.services.impl.PositionInScheduleServiceImpl;
@@ -14,5 +18,11 @@ public class PositionInScheduleController {
 	private final PositionInScheduleServiceImpl positionInScheduleService;
 	public PositionInScheduleController(PositionInScheduleServiceImpl positionInScheduleService) {
 		this.positionInScheduleService=positionInScheduleService;
+	}
+	
+	@PutMapping
+	PositionInSchedule update(@RequestBody PositionInSchedule position) {
+		PositionInSchedule updated = positionInScheduleService.updatePositionInSchedule(position); 
+		return updated;
 	}
 }
