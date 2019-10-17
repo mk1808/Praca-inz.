c2786005d995804a015da09718eb5369f2a94649
Monika Kordoń <monikor97@gmail.com>
2019-10-17 17:03:52 +0200

    #27 dodanie wylogowywania

--- View/inz-app/src/app/inz/layout/header/header.component.ts
+++ View/inz-app/src/app/inz/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +9,21 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 status:boolean;
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router: Router) { }
 
   ngOnInit() {
     this.auth.currentStatus.subscribe(status=>{
       this.status=status;
       console.log(this.status);
-      this.status=true;
+     // this.status=true;
     })
 
 
+  }
+
+  onLogOut(){
+    this.auth.changeStatus(false);
+    this.router.navigate(['/'])
   }
 
 }
