af2eaed4ad92df3eb17e9a9ccb47afe86305d5cd
Monika Kordoń <monikor97@gmail.com>
2019-10-18 12:14:12 +0200

    #28 wyswietlanie pina na mapie

--- View/inz-app/src/app/test/test.component.ts
+++ View/inz-app/src/app/test/test.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Feature } from 'openlayers';
+import { MatTooltip } from '@angular/material';
 @Component({
     selector: 'app-test',
     templateUrl: './test.component.html',
@@ -9,8 +11,9 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 export class TestComponent implements OnInit {
      ol: any;
      map:any;
-
+     @ViewChild('tooltip') tooltip:MatTooltip;
     keyword = 'name';
+    visible:boolean=false;
   data = [
      {
        id: 1,
@@ -22,7 +25,9 @@ export class TestComponent implements OnInit {
      }
   ];
  
-  
+  public zoom = 15;
+  public opacity = 1.0;
+  public width = 5;
   
  
   selectEvent(item) {
@@ -118,29 +123,132 @@ export class TestComponent implements OnInit {
 
     ngOnInit(): void {
 
+
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([ 22.0025522,50.0333997 ])),
+            name: 'Null Island',
+            population: 4000,
+            rainfall: 500
+          });
+          
+          var iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+                size:[100,120],
+            /*  anchor: [0, 0],
+              anchorXUnits: 'pixels',
+              anchorYUnits: 'pixels',*/
+              anchor: [14, 38],
+              anchorXUnits: 'pixels',
+              anchorYUnits: 'pixels',
+              src: 'assets/placeholder2.png',
+              
+            })
+          });
+          
+          iconFeature.setStyle(iconStyle);
+          
+          var vectorSource = new ol.source.Vector({
+            features: [iconFeature]
+          });
+          
+          var vectorLayer = new ol.layer.Vector({
+            source: vectorSource
+          });
+          
+var element:any = document.getElementById('popup');
+          
+          var popup = new ol.Overlay({
+            element: element,
+            positioning: 'bottom-center',
+            stopEvent: false,
+            offset: [0, -50]
+          });
+     
         this.map = new ol.Map({
             target: 'map',
             layers: [
               new ol.layer.Tile({
                 source: new ol.source.OSM()
-              })
+              }),vectorLayer
             ],
             view: new ol.View({
               center: ol.proj.fromLonLat([21, 52]),
               zoom: 8
             })
           });   
+          this.map.addOverlay(popup);
+          var layer = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: [
+                    new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.fromLonLat([4.35247, 50.84673]))
+                    })
+                ]
+            })
+        });
+       
+        this.map.addLayer(layer);
+/*
+        var source = new ol.source.Vector({});
+var layer2 = new ol.layer.Vector({ source: source});
+    this.map.addLayer(layer2);
 
-          this.map.on('click', function (args) {
+    var marker = new ol.Feature({
+        geometry: new ol.geom.Point([0,0]) // dont worry about coordinate type 0,0 will be in west coast of africa
+    }); 
+
+    source.addFeature(marker);*/
+
+      /*   this.map.on('click', function (args) {
             console.log(args.coordinate);
             var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
             console.log(lonlat);
             
             var lon = lonlat[0];
             var lat = lonlat[1];
-            alert(`lat: ${lat} long: ${lon}`);
+            this.tooltip.toggle();
+           // alert(`lat: ${lat} long: ${lon}`);
           });
-          
+        */
+         //////////////
+         this.map.on('click', (evt) =>{
+            var feature =this.map.forEachFeatureAtPixel(evt.pixel,
+              function(feature) {
+                return feature;
+              });
+            if (feature) {
+              var coordinates = feature.getGeometry().getCoordinates();
+              console.log(coordinates)
+              popup.setPosition(coordinates);
+                this.visible=!this.visible;
+              this.tooltip.toggle();
+           /*   popup.popover({
+                placement: 'top',
+                html: true,
+                content: feature.get('name')
+              });*/
+           // element.popover('show');
+            } else {
+                this.tooltip.hide();
+             // element.popover('destroy');
+            }
+          });
+       
+       
+       
+         
+        
+
+
+
+
+
+
+
+
+
+
+
         this.galleryOptions = [
             {
                 width: '600px',
@@ -190,5 +298,24 @@ export class TestComponent implements OnInit {
         ];
     }
 
-   
+    increaseZoom() {
+        this.zoom  = Math.min(this.zoom + 1, 18);
+        console.log('zoom: ', this.zoom);
+      }
+    
+      decreaseZoom() {
+        this.zoom  = Math.max(this.zoom - 1, 1);
+        console.log('zoom: ', this.zoom);
+      }
+    
+      increaseOpacity() {
+        this.opacity  = Math.min(this.opacity + 0.1, 1);
+        console.log('opacity: ', this.opacity);
+      }
+    
+      decreaseOpacity() {
+        this.opacity  = Math.max(this.opacity - 0.1, 0);
+        console.log('opacity: ', this.opacity);
+      }
+
 }
