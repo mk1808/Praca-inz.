295e877051b098f1fda95daf0cc116ec3f179f54
Monika Kordoń <monikor97@gmail.com>
2019-10-21 18:10:41 +0200

    #30 tworzenie tablicy godzin dla wszystkich miejsc przy wyswietlaniu grafiku

--- View/inz-app/src/app/inz/trip/schedule/schedule.component.ts
+++ View/inz-app/src/app/inz/trip/schedule/schedule.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TripService } from 'src/app/shared/services/trip.service';
-import { Trip, Schedule, PositionInSchedule } from 'src/app/shared/models/classes';
+import { Trip, Schedule, PositionInSchedule, Place } from 'src/app/shared/models/classes';
 import { ScheduleService } from 'src/app/shared/services/schedule.service';
+import { ComponentsService } from 'src/app/shared/services/components.service';
 
 @Component({
   selector: 'app-schedule',
@@ -16,12 +17,14 @@ export class ScheduleComponent implements OnInit {
   positions:PositionInSchedule[][]=[];
   allPositions:PositionInSchedule[][]=[];
   positionsWODay:PositionInSchedule[]=[];
+  tabOpeningHours:any[]=[];
+  places:Place[]=[];
   allDaysSortedFinal: any[] = [{ start: null, end: null, ids: [] }, { start: null, end: null, ids: [] }, { start: null, end: null, ids: [] },
   { start: null, end: null, ids: [] }, { start: null, end: null, ids: [] }, { start: null, end: null, ids: [] }, { start: null, end: null, ids: [] }];
  
 
   constructor(private router: Router, private route: ActivatedRoute, private tripService: TripService,
-    private scheduleService: ScheduleService) { }
+    private scheduleService: ScheduleService,  private componentService:ComponentsService) { }
 
   ngOnInit() {
 
@@ -30,9 +33,21 @@ export class ScheduleComponent implements OnInit {
       this.tripService.getTrip(this.id).subscribe(y => {
         this.trip = y;
         this.schedule=this.trip.schedule;
+       });
+      
+
+    this.tripService.getPlacesForTrip(this.id).subscribe(z=>{
+      this.places=z;
+      this.places.forEach(position=>{
+        this.tabOpeningHours[position.name]=this.componentService.getHoursForDays(position.hours);
+    })
+    console.log(this.tabOpeningHours);
+    })    
+        
+
      //   this.positions=this.trip.
       //  console.log(this.schedule);
-      });
+     
      /* this.scheduleService.getPositionsForScheduleByTrip(this.id).subscribe(x=>{
         this.positions=x;
        
