a0fe2d55b6b8664a8678a0a5053c8c5be21b1500
Monika Kordoń <monikor97@gmail.com>
2019-10-23 20:45:27 +0200

    #30 dest-searching - formularz miejsce

--- View/inz-app/src/app/inz/main/destination-searching-results/destination-searching-results.component.ts
+++ View/inz-app/src/app/inz/main/destination-searching-results/destination-searching-results.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PlaceService } from 'src/app/shared/services/place.service';
+import { Place } from 'src/app/shared/models/classes';
+import { DictionaryService } from 'src/app/shared/services/dictionary.service';
 
 @Component({
   selector: 'app-destination-searching-results',
@@ -10,7 +13,10 @@ export class DestinationSearchingResultsComponent implements OnInit {
   form: FormGroup;
   formTrip: FormGroup;
   initialized=false;
-  constructor( private fb: FormBuilder) { }
+  filteredPlaces: Place[];
+  filteredRegions: String[];
+  categories: String[];
+  constructor( private fb: FormBuilder, private placeService: PlaceService, private dictionaryService: DictionaryService) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -26,6 +32,34 @@ export class DestinationSearchingResultsComponent implements OnInit {
       region: [""]
     });
     this.initialized = true;
+
+    
+    this.form.controls.name.valueChanges.subscribe(y => {
+      console.log(y);
+      this.placeService.getPlacesFiltered(y).subscribe(x => {
+        this.filteredPlaces = x;
+        console.log(this.filteredPlaces);
+
+      })
+
+    })
+
+
+    this.form.controls.region.valueChanges.subscribe(z => {
+      console.log(z);
+      this.placeService.getRegionsFiltered(z).subscribe(a => {
+        this.filteredRegions = a;
+        console.log(this.filteredRegions);
+
+      })
+
+    })
+
+    
+    this.dictionaryService.getCategories().subscribe(x => {
+      this.categories = x;
+      console.log(this.categories);
+    })
   }
 
 }
