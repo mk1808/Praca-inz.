404da10198258a712013bd5c8e98aa0bd2218e7d
Monika Kordoń <monikor97@gmail.com>
2019-10-23 20:45:46 +0200

    #30 dest-searching - formularz wycieczka

--- View/inz-app/src/app/inz/main/destination-searching-results/destination-searching-results.component.ts
+++ View/inz-app/src/app/inz/main/destination-searching-results/destination-searching-results.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { PlaceService } from 'src/app/shared/services/place.service';
 import { Place } from 'src/app/shared/models/classes';
 import { DictionaryService } from 'src/app/shared/services/dictionary.service';
+import { TripService } from 'src/app/shared/services/trip.service';
+import { MatChipInputEvent, MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material';
 
 @Component({
   selector: 'app-destination-searching-results',
@@ -15,8 +17,15 @@ export class DestinationSearchingResultsComponent implements OnInit {
   initialized=false;
   filteredPlaces: Place[];
   filteredRegions: String[];
+  filteredRegionsTrip: String[];
+  filteredTags: String[];
+  chosenTags:String[]=[];
   categories: String[];
-  constructor( private fb: FormBuilder, private placeService: PlaceService, private dictionaryService: DictionaryService) { }
+
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  constructor( private fb: FormBuilder, private placeService: PlaceService, 
+    private tripService: TripService, private dictionaryService: DictionaryService) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -60,6 +69,61 @@ export class DestinationSearchingResultsComponent implements OnInit {
       this.categories = x;
       console.log(this.categories);
     })
+
+    this.formTrip.controls.region.valueChanges.subscribe(z => {
+      console.log(z);
+      this.tripService.getRegionsFiltered(z).subscribe(a => {
+        this.filteredRegionsTrip= a;
+        console.log(this.filteredRegionsTrip);
+
+      })
+  })
+
+  this.formTrip.controls.tags.valueChanges.subscribe(y => {
+    console.log(y);
+    this.dictionaryService.getTags().subscribe(x => {
+      this.filteredTags = x;
+      console.log(this.filteredTags);
+
+    })
+})
+
+
   }
 
+  add(event: MatChipInputEvent): void {
+
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      if ((value || '').trim()) {
+        this.chosenTags.push(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.form.controls.tags.setValue(null);
+    }
+  }
+
+  remove(fruit: string): void {
+    const index = this.chosenTags.indexOf(fruit);
+
+    if (index >= 0) {
+      this.chosenTags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.chosenTags.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.form.controls.tags.setValue(null);
+
+  }
 }
+
+
