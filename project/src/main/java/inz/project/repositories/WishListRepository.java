2b3e46eb24d682d73aa879721eabef6a701a914a
Monika Kordoń <monikor97@gmail.com>
2019-10-28 16:39:35 +0100

    #33 encja wishlist -cd

---
+++ project/src/main/java/inz/project/repositories/WishListRepository.java
@@ -0,0 +1,9 @@
+package inz.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import inz.project.models.WishList;
+
+public interface WishListRepository extends JpaRepository<WishList, Long> {
+
+}
