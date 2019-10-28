c9075c9ce9e931d72109e1b11223fef2c32aa93f
Monika Kordoń <monikor97@gmail.com>
2019-10-28 17:00:47 +0100

    #33 zwracanie listy dla uzytkownika

--- project/src/main/java/inz/project/repositories/WishListRepository.java
+++ project/src/main/java/inz/project/repositories/WishListRepository.java
@@ -1,10 +1,13 @@
 package inz.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import inz.project.models.User;
 import inz.project.models.WishList;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 
-
+	List<WishList> getWishListByUser(User user);
 }
--- project/src/main/java/inz/project/services/WishListService.java
+++ project/src/main/java/inz/project/services/WishListService.java
@@ -1,10 +1,13 @@
 package inz.project.services;
 
+import java.util.List;
+
+import inz.project.models.User;
 import inz.project.models.WishList;
 
 public interface WishListService {
 	
 	WishList createWishList(WishList list);
 	void deleteWishList(Long id);
-	
+	List<WishList> getWishListByUser(Long id);
 }
