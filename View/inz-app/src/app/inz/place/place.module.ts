eb63009472b252ed129c208641c7fbfc58284b89
mk1808 <monikor97@gmail.com>
2019-11-11 19:30:09 +0100

    #32 dodanie przycisku do oceny na wid. miejsca

--- View/inz-app/src/app/inz/place/place.module.ts
+++ View/inz-app/src/app/inz/place/place.module.ts
@@ -8,7 +8,7 @@ import { AgmCoreModule } from '@agm/core';
 import { NgxEqualizerModule } from 'ngx-equalizer';
 import { NewPlaceComponent } from './new-place/new-place.component';
 import { AddPlaceComponent } from './add-place/add-place.component';
-import { MatDialogModule, MatSelectModule, MatFormFieldModule, MatAutocompleteModule, MatChipsModule, MatCheckboxModule, MatTooltip, MatTooltipModule } from '@angular/material';
+import { MatDialogModule, MatSelectModule, MatFormFieldModule, MatAutocompleteModule, MatChipsModule, MatCheckboxModule, MatTooltip, MatTooltipModule, MatButtonModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { ImageUploadModule } from 'angular2-image-upload';
@@ -40,7 +40,8 @@ import { SharedModule } from 'src/app/shared/shared/shared.module';
     MatTooltipModule,
     MatCardModule,
     MatDividerModule,
-    SharedModule
+    SharedModule,
+    MatButtonModule
   ], 
   entryComponents: [
 
