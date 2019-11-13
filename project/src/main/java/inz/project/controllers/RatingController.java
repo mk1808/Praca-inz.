0ca5f02455dab28184bd8aaabf27529a9fdfbea8
mk1808 <monikor97@gmail.com>
2019-11-13 02:08:12 +0100

    #32 kontroller rating

---
+++ project/src/main/java/inz/project/controllers/RatingController.java
@@ -0,0 +1,11 @@
+package inz.project.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rating")
+public class RatingController {
+
+	
+}
