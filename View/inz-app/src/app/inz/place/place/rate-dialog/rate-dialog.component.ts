4d66a68d8c2ad063b3d79d83a90ebf689e3ac94a
mk1808 <monikor97@gmail.com>
2019-11-13 02:41:51 +0100

    #32 podpiecie formularza

--- View/inz-app/src/app/inz/place/place/rate-dialog/rate-dialog.component.ts
+++ View/inz-app/src/app/inz/place/place/rate-dialog/rate-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { Rating } from 'src/app/shared/models/classes';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-rate-dialog',
@@ -8,11 +10,24 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class RateDialogComponent implements OnInit {
 initialized=false;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+rating:Rating= new Rating;
+form: FormGroup;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fb:FormBuilder) { }
 
   ngOnInit() {
     console.log(this.data);
+    this.form = this.fb.group({
+      rating:['']
+    })
     this.initialized=true;
+  }
+
+  onConfirm(){
+  this.rating.place=this.data.place;
+  this.rating.trip = this.data.trip;
+  this.rating.value=this.form.controls.rating.value;
+  console.log(this.rating);
+    
   }
 
 }
