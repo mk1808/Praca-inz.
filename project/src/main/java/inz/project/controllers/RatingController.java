9c9e18d1f7f5235249c42acc1e96f770fd44ccd5
mk1808 <monikor97@gmail.com>
2019-11-13 13:30:39 +0100

    #32 sprawdzanie, czy uzytkownik ocenil juz miejsce/wycieczke

--- project/src/main/java/inz/project/controllers/RatingController.java
+++ project/src/main/java/inz/project/controllers/RatingController.java
@@ -2,9 +2,11 @@ package inz.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,16 @@ public class RatingController {
 		return ratingService.createRating(rating);
 	}
 	
+	
+	@GetMapping("/trip")
+	Rating getRatingByTripAndUser(@RequestParam(value ="trip") Long trip,
+			@RequestParam(value ="user") Long user) {
+		return ratingService.getRatingByTripAndUser(trip, user);	
+	}
+	
+	@GetMapping("/place")
+	Rating getRatingByPlaceAndUser(@RequestParam(value ="place") Long place,
+			@RequestParam(value ="user") Long user) {
+		return ratingService.getRatingByPlaceAndUser(place, user);				
+	}
 }
