6cbd2b5292e4b77aedea632fde7508e7dc2ecd18
mk1808 <monikor97@gmail.com>
2019-11-19 16:02:42 +0100

    #36 niezatwierdzone miejsca - front

--- View/inz-app/src/app/inz/other/not-conifrmed-places/not-conifrmed-places.component.ts
+++ View/inz-app/src/app/inz/other/not-conifrmed-places/not-conifrmed-places.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip, Place, User } from 'src/app/shared/models/classes';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TripService } from 'src/app/shared/services/trip.service';
+import { PlaceService } from 'src/app/shared/services/place.service';
+import { ComponentsService } from 'src/app/shared/services/components.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-not-conifrmed-places',
@@ -6,10 +12,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-conifrmed-places.component.scss']
 })
 export class NotConifrmedPlacesComponent implements OnInit {
-
-  constructor() { }
-
+  hover = false;
+  myTrips: Trip[]=[];
+  myPlaces: Place[]=[];
+  myFavTrips:Trip[]=[];
+  placesFromTrip:Place[]=[];
+  user: User;
+  res:any;
+  constructor(private router: Router, private route: ActivatedRoute, private cookie: CookieService,
+    private tripService: TripService, private placeService:PlaceService, 
+    private componentService:ComponentsService) {}
   ngOnInit() {
+    if (this.cookie.get('user') == "") {
+      this.router.navigate(['/']);
+
+    } else {
+      this.user = JSON.parse(this.cookie.get('user'));
+      this.tripService.getTripsByUser(this.user.id).subscribe(x => {
+        this.myTrips = x;
+        console.log(this.myTrips);
+
+      })
+
+
+      this.placeService.getPlacesByUser(this.user.id).subscribe(x=>{
+        
+        this.myPlaces=x;
+        let i=0;
+        this.myPlaces.forEach(x=>{
+          this.myPlaces[i].category=this.componentService.changeCategoriesToDisplay(this.myPlaces[i].category);
+          i++;
+        })
+        console.log(this.myPlaces);
+
+      })
+      
+      this.tripService.getWishListsForUser(this.user.id).subscribe(x=>{
+       console.log(x);
+
+        x.forEach(element => {
+         this.myFavTrips.push(element.trip); 
+     
+ 
+      })
+    }
+
+      )
   }
 
+}
 }
