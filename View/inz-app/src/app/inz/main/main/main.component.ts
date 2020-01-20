3ca2dba7c99c7d02b67fd73c791317b8a39be252
mk1808 <monikor97@gmail.com>
2020-01-20 12:06:54 +0100

    #34 poprawienie obrazków na stronie glownej

--- View/inz-app/src/app/inz/main/main/main.component.ts
+++ View/inz-app/src/app/inz/main/main/main.component.ts
@@ -18,7 +18,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class MainComponent implements OnInit {
    
   imageSources:string[]=['https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/London_Eye_at_night_5.jpg/1280px-London_Eye_at_night_5.jpg', 'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2018/06/ultra-wide-mistakes-lead-image.jpg',
-'https://media.fshoq.com/images/150/arts-museum-louvre-in-paris-during-the-night-150-medium.jpg'];
+'https://media.fshoq.com/images/150/arts-museum-louvre-in-paris-during-the-night-150-medium.jpg',
+'https://media.fshoq.com/images/53/polish-sea-baltic-53-medium.jpg'];
   places:Place[];
   trips:Trip[];
   initializedPlaces = false;
